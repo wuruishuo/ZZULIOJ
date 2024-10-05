9fc7a7f45caa092472e528865ccdcf4345cfe451
wuruishuo <3179544063@qq.com>
2024-10-05 18:00:09 +0800

    first commit

---
+++ 0000/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _0001;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("从今天开始入坑C语言");
+    }
+}
---
+++ 0001/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace _0001;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    Console.WriteLine("换行输入/不换行输入");
+    int  a = Convert.ToInt32(Console.ReadLine());
+    int  b = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("换行输入:"+(a+b));
+
+    string shuzi = Console.ReadLine();
+    int c = Convert.ToInt32(shuzi.Substring(0, shuzi.IndexOf(" ")));
+    int d = Convert.ToInt32(shuzi.Substring(shuzi.LastIndexOf(" ")));
+    Console.WriteLine("不换行输入:"+(c+d));
+    }
+}
