602161d27bbf7c4d7ea0cb4dbce837fa79b001b0
wuruishuo <3179544063@qq.com>
2024-10-06 14:09:08 +0800

    数据更新windows

--- 0000/obj/Debug/net8.0/0000.AssemblyInfo.cs
+++ 0000/obj/Debug/net8.0/0000.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("0000")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36289fdce5ba33f2f28b6390d714e2107b891321")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22f41870bc11e39ad9cbb00fef96eade56eb555f")]
 [assembly: System.Reflection.AssemblyProductAttribute("0000")]
 [assembly: System.Reflection.AssemblyTitleAttribute("0000")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
