439eab9d59360a13526f1efa85019bb4252bad01
wuruishuo <3179544063@qq.com>
2024-10-06 20:48:05 +0800

    数据更新windows

--- 0000/obj/Debug/net8.0/0000.AssemblyInfo.cs
+++ 0000/obj/Debug/net8.0/0000.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("0000")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22f41870bc11e39ad9cbb00fef96eade56eb555f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+602161d27bbf7c4d7ea0cb4dbce837fa79b001b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("0000")]
 [assembly: System.Reflection.AssemblyTitleAttribute("0000")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ 0004/三位数的数位分离.cs
@@ -0,0 +1,14 @@
+﻿namespace _0004;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num=Convert.ToInt32(Console.ReadLine());
+        int ge, shi, bai;
+        bai = num / 100;
+        shi = (num % 100) / 10;
+        ge = num % 10;
+        Console.WriteLine(ge+" "+shi+" "+bai);
+    }
+}
