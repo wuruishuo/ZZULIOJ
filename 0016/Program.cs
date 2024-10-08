d17fee1aa46bbe2bad258ccc7a58aa0e5d163633
wuruishuo <3179544063@qq.com>
2024-10-08 21:35:31 +0800

    linux 更新

--- 0016/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _0016;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
---
+++ 0016/银行利率.cs
@@ -0,0 +1,14 @@
+﻿namespace _0016;
+
+class Program
+{
+    const double L=2.25;
+    static void Main(string[] args)
+    {
+        string[] input=Console.ReadLine().Split();
+        int year=Convert.ToInt32(input[0]);
+        double capital=Convert.ToDouble(input[1]);
+        double money=Math.Pow((1+L/100),year)*capital;
+        Console.WriteLine($"{money:f6}");
+    }
+}
